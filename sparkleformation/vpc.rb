2fb178220e8c841b91e166a7d74465aea10248c0
Michael F. Weinberg <michael@hw-ops.com>
2015-09-07 10:24:49 -0700

    Use AZ index to set subnet CIDR default. Add explanatory comments. VPC should now have all the defaults it needs.

--- sparkleformation/vpc.rb
+++ sparkleformation/vpc.rb
@@ -2,11 +2,23 @@ SparkleFormation.new(:hw_training_vpc).load(:base, :vpc).overrides do
 
   description 'Basic VPC for Heavy Water SparkleFormation Workshop'
   
-  dynamic!(:subnet, ['public_', zone.gsub('-', '_') ].join,
-           :vpc_id => ref!(:vpc),
-           :route_table => ref!(:public_route_table),
-           :availability_zone => zone
-           )
-  end
+  ## Access the list of available Availability Zones via registry entry.
+  zones = registry!(:zones)
 
+  ## Iterate over each AZ creating a public subnet. Auto-generate
+  ## Subnet CIDRs based on index.
+  zones.each_with_index do |zone, index|
+
+    dynamic!(:subnet, ['public_', zone.gsub('-', '_') ].join,
+             :vpc_id => ref!(:vpc),
+             :route_table => ref!(:public_route_table),
+             :availability_zone => zone
+             )
+
+    parameters(['public_', zone.gsub('-', '_'), '_subnet_cidr' ].join) do
+      default ['10.0.', index, '.0/24'].join
+    end
+
+  end
+ 
 end
